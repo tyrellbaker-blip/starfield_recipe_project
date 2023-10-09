14f13bdaaf42060bef24498a049aa2013b503ecd
Tyrell Baker <tybaker1@csumb.edu>
2023-10-08 23:34:56 -0700

    Implemented some fields and set up a couple models. See TODO

--- recipes/models.py
+++ recipes/models.py
@@ -1,15 +1,21 @@
 from django.db import models
 
-
+#TODO: I want to add in some functionality. Ideally, I want to be able to choose an item from an alphabetized
+# dropdown menu and have the ingredients used to make the item displayed. If the ingredients are not organic or
+# inorganic material, then I want to be able to link to the recipes for the items to see what they need to be
+# crafted. It would be nice to code in the functionality to make some kind of 'cart.' You say all of the things you
+# want to build, and the website snaps you to a page where you can see the ingredients, what planets you can find
+# them on, how much total power/storage you'll need to create them, etc. Continue with the idea from here,
+# implement bit by bit. CHANGE THE BRANCH!!!
 class Ingredient(models.Model):
-    name = models.CharField()
+    name = models.CharField(max_length=100)
 
     def __str__(self):
         return self.name
 
 
 class Recipe(models.Model):
-    name = models.CharField()
+    name = models.CharField(max_length=100)
     ingredients = models.ManyToManyField(Ingredient, through='RecipeIngredient')
 
     def __str__(self):
